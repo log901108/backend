928ac4aa8c202fbc271ce2c5b19165f01708cc88
log901108 <log901108@gmail.com>
2020-07-10 23:57:46 +0000

    dev:1st commit

--- routes/api/auth/middle/maccesstoken.js
+++ routes/api/auth/middle/maccesstoken.js
@@ -15,7 +15,7 @@ module.exports = async (req, res, next) => {
         //TODO access token은 json으로 넘기고 refresh token은 cookie에 박도록 바꾸기
         //TODO https://velog.io/@yaytomato/%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%90%EC%84%9C-%EC%95%88%EC%A0%84%ED%95%98%EA%B2%8C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0
         req.accesstoken = data;
-        //console.log(req.accesstoken);
+        console.log(req.accesstoken);
       });
       return next();
     } catch (e) {
